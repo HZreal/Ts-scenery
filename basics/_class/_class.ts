ae7f587b07942cdf7501be09aa628ae864b2c0a9
HZreal <hzreal0823@gmail.com>
2024-03-16 12:27:41 +0800

    chore: adjust directory structure

--- basics/_class/_class.ts
+++ basics/_class/_class.ts
@@ -3,22 +3,19 @@
  * @date 2023-10-27
  */
 class DemoClass {
-    static a: number // 静态属性，无需初始化
+  static a: number; // 静态属性，无需初始化
 
-    // b: number // 报错：Property 'b' has no initializer and is not definitely assigned in the constructor.
+  // b: number // 报错：Property 'b' has no initializer and is not definitely assigned in the constructor.
 
-    c!: number // ! 表示该变量会在构造函数之外的地方被初始化，否则会报错
-
+  c!: number; // ! 表示该变量会在构造函数之外的地方被初始化，否则会报错
 }
 
-DemoClass.a = 2
-console.log('  ---->  ', DemoClass.a);
+DemoClass.a = 2;
+console.log("  ---->  ", DemoClass.a);
 
-const demoObj = new DemoClass()
+const demoObj = new DemoClass();
 // TODO 待考究
-const customSetter = ()=>{}
-Object.defineProperty(DemoClass, 'c', {set(v: any) {
-    }})
-demoObj.c = 1 // 实际是调用类该属性的 set 方法
-const b= demoObj.c // 实际是调用类该属性的 get 方法
-
+const customSetter = () => {};
+Object.defineProperty(DemoClass, "c", { set(v: any) {} });
+demoObj.c = 1; // 实际是调用类该属性的 set 方法
+const b = demoObj.c; // 实际是调用类该属性的 get 方法
